585280bf5b9d027efc901c3b84b718185cb35e13
Dulak <110743178+BSCDulak@users.noreply.github.com>
2025-02-23 17:05:23 +0100

    added some content to experiment

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,4 +20,27 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        ItemList.Items.Add(UserInput.Text);
+        UserInput.Clear();
+        UserInput.Focus();
+    }
+
+    private void AddViaEnter(object sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            AddButton_Click(sender, e);
+        }
+    }
+
+    private void DeleteSelectedButton_Click(object sender, RoutedEventArgs e)
+    {
+        while (ItemList.SelectedItems.Count > 0)
+        {
+            ItemList.Items.Remove(ItemList.SelectedItem);
+        }
+    }
 }
